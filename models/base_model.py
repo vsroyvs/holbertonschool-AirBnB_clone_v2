104597df274554f0f9dc97ad54577d78fa8e9c52
Roy <vargas.sap321@gmail.com>
2023-09-16 23:02:08 -0700

    Fix: TypeError

--- models/base_model.py
+++ models/base_model.py
@@ -53,8 +53,7 @@ class BaseModel:
                 if '__class__' not in key:
                     setattr(self, key, value)
 
-            if not self.id:
-                self.id = str(uuid.uuid4())
+            self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
