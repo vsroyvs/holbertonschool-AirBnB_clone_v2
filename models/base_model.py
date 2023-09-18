06b1d7758800cde79d747bd6c626e709b89f2c36
Roy <vargas.sap321@gmail.com>
2023-09-18 00:20:52 -0500

    Fix: fixing created attributes depend of the storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,11 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 import models
+from os import getenv
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
+
 
 Base = declarative_base()
 
@@ -12,48 +14,36 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(
-            String(60),
-            primary_key=True,
-            nullable=False)
-
-    created_at = Column(
-            DateTime,
-            nullable=False,
-            default=datetime.utcnow())
-
-    updated_at = Column(
-            DateTime,
-            nullable=False,
-            default=datetime.utcnow())
-
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        else:
-            if kwargs.get("updated_at"):
-                kwargs['updated_at'] = datetime.strptime(
-                        kwargs['updated_at'],
-                        '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                self.updated_at = datetime.now()
-
-            if kwargs.get("created_at"):
-                kwargs['created_at'] = datetime.strptime(
-                        kwargs['created_at'],
-                        '%Y-%m-%dT%H:%M:%S.%f')
-            else:
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        id = Column(String(60), primary_key=True, nullable=False,
+                    default=lambda: str(uuid.uuid4()))
+        created_at = Column(DateTime, default=datetime.utcnow,
+                            nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow,
+                            nullable=False)
+    else:
+        def __init__(self, *args, **kwargs):
+            """Instatntiates a new model"""
+            if not kwargs:
+                self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
+                self.updated_at = self.created_at
+            else:
+                if kwargs.get("created_at"):
+                    kwargs["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.created_at = datetime.now()
+                if kwargs.get("created_at"):
+                    kwargs["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.updated_at = datetime.now()
+                for key, value in kwargs.items():
+                    if "__class__" not in key:
+                        setattr(self, key, value)
 
-            for key, value in kwargs.items():
-                if '__class__' not in key:
-                    setattr(self, key, value)
-
-            self.id = str(uuid.uuid4())
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
