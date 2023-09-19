982eb799aa9b9bc8978150497378abcf45986cc0
Roy <vargas.sap321@gmail.com>
2023-09-19 18:33:12 -0500

    feat: Add route /city_by_states to display all states with its cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module to starts a Flask Web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(exception):
+    """ Close the Session """
+    storage.close()
+
+
+@app.route('/city_by_states', strict_slashes=False)
+def city_by_states():
+    """ List all states on a HTML page """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    
+    return render_template('8-cities_by_states.html', states=states,
+                           cities=cities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
